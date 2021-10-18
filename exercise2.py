84735d0c11ab57e8939d6d8a11b1127f1b3e7f9c
juanm <jbaader@itba.edu.ar>
2021-10-18 18:24:59 -0300

    get data ej2

--- exercise2.py
+++ exercise2.py
@@ -1,1 +1,24 @@
+import pandas as pd
+from PIL import Image
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+import numpy as np
+import cv2
 
+def get_data():
+    dic = {"r": [], "g": [], "b": [], "class_type": []}
+    for file, clase in [["cielo.jpg", 0], ["pasto.jpg", 1], ["vaca.jpg", 2]]:
+        var = np.asarray(Image.open("resources/" + file))
+        print(var[0][0])
+        for i in range(len(var)):
+            for j in range(len(var[i])):
+                dic["r"].append(var[i][j][0] + 0)
+                dic["g"].append(var[i][j][1] + 0)
+                dic["b"].append(var[i][j][2] + 0)
+                dic["class_type"].append(clase)
+    data = pd.DataFrame(dic)
+    sample = data.sample(frac=0.10)
+    return data, sample
+
+
+test_data, train_data = get_data()
